a8d267d5199a484f425d9783d0bf997c6a8350d5
alecburnett <60477996+alecburnett@users.noreply.github.com>
2021-07-02 11:26:23 +0100

    Update Desktop_Tidy.py

--- Desktop_Tidy.py
+++ Desktop_Tidy.py
@@ -1,100 +1,49 @@
-other           = ['.xml', '.log', '.recover', '.writingout']
-pdf_documents   = ['.pdf', '.pptx', '.docx', '.txt']
-images          = ['.jpeg','.jpg','.svg','.png','.PNG','.mp4','.mp3', '.tif', '.JPG']
-excels          = ['.xlsx', 'xls', '.csv']
-shapefiles      = ['.gpkg', '.shp']
-python          = ['.py', '.pynb', '.ipynb']
-sql             = ['.sql']
-fme             = ['.fmw']
-qgis            = ['.qgs','.qgz']
-disk_images     = ['.dmg', '.app']
-zip_files       = ['.zip']
-
-categories = 
-
-#define your main location here:
-main_location   = r'/Users/alexburnett/Downloads'
-
-pdfLocation=r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\PDFs'
-shapeLocation=r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\Shapefiles'
-excelFilesLocation=r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\Excels'
-imageFilesLocation=r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\Images'
-fmeLocation=r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\FME'
-qgisLocation=r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\QGIS'
-otherLocation = r'C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\Other'
-
-
-def sorter(file):
-    try:
-        print(file)
-#pdf
-        if os.path.splitext(file)[1] in PDF:
-            if(path.exists(pdfLocation)):
-                shutil.move(file,pdfLocation)
-            else:
-                os.mkdir(pdfLocation)
-                shutil.move(file,pdfLocation)
-#shape
-        if os.path.splitext(file)[1] in shapefiles:
-            if(path.exists(shapeLocation)):
-                shutil.move(file,shapeLocation)
-            else:
-                os.mkdir(shapeLocation)
-                shutil.move(file,shapeLocation)
-#excel
-        if os.path.splitext(file)[1] in excels:
-            if(path.exists(excelFilesLocation)):
-                shutil.move(file,excelFilesLocation)
-            else:
-                os.mkdir(excelFilesLocation)
-                shutil.move(file,excelFilesLocation)
-#images
-        if os.path.splitext(file)[1] in Images:
-            if(path.exists(imageFilesLocation)):
-                shutil.move(file,imageFilesLocation)
-            else:
-                os.mkdir(imageFilesLocation)
-                shutil.move(file,imageFilesLocation)
-#fme
-        if os.path.splitext(file)[1] in fme:
-            if(path.exists(fmeLocation)):
-                shutil.move(file,fmeLocation)
-            else:
-                os.mkdir(fmeLocation)
-                shutil.move(file,fmeLocation)
-
-#QGIS
-        if os.path.splitext(file)[1] in qgis:
-            if(path.exists(qgisLocation)):
-                shutil.move(file,qgisLocation)
-            else:
-                os.mkdir(qgisLocation)
-                shutil.move(file,qgisLocation)
-#other
-        if os.path.splitext(file)[1] in other:
-            if(path.exists(otherLocation)):
-                shutil.move(file,otherLocation)
-            else:
-                os.mkdir(otherLocation)
-                shutil.move(file,otherLocation)
-    except:
-        print('failed')
-        pass
-    
-
 import os
 import glob
 import shutil
 from os import path
-desktop = glob.glob(r"C:\Users\AlexanderBurnett\Desktop\*")
-dropbox = glob.glob(r"C:\Users\AlexanderBurnett\Dropbox (MapStand)\MapStand Team Folder\Alex Burnett\Various\*")
 
+#define buckets
+v_other           = ['.xml', '.log', '.recover', '.writingout', '.crdownload']
+v_pdf_documents   = ['.pdf', '.pptx', '.docx', '.txt']
+v_images          = ['.jpeg','.jpg','.svg','.png','.PNG','.mp4','.mp3', '.tif', '.JPG']
+v_excels          = ['.xlsx', 'xls', '.csv']
+v_shapefiles      = ['.gpkg', '.shp']
+v_python          = ['.py', '.pynb', '.ipynb']
+v_sql             = ['.sql']
+v_fme             = ['.fmw']
+v_qgis            = ['.qgs','.qgz']
+v_disk_images     = ['.dmg', '.app']
+v_zip_files       = ['.zip']
+v_vpn             = ['.ovpn']
 
-print("desktop")
-for file in desktop:
-    sorter(file)
+#define your main location here:
+main_location   = r'/Users/alexburnett/Downloads'
 
-print("dropbox")
-for file in dropbox:
-    sorter(file)
+#sorter function
+def sorter(file, value, folder):
+    try:
+        print(file)
+        if os.path.splitext(file)[1] in value:
+            if (path.exists(folder)):
+                shutil.move(file, folder)
+            else:
+                os.mkdir(folder)
+                shutil.move(file, folder)
+    except:
+      pass
 
+#extract files
+files = glob.glob(str(main_location) + '''/*''')
+
+#loop through files
+for file in files:
+  print(file)
+
+  #loop through variables and pass files through sorter functino
+  for variable in dir():
+    if variable.startswith('v_'):
+      value = eval(variable)
+      folder =  ( str(main_location) + '/' + str(variable).replace("v_", "") )
+      print(str(value) + str(folder))
+      sorter(file, value, folder)
